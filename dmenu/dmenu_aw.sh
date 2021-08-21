f3052394771dd2a94ef59db028a88e6d4ed0cd3d
Yo3lB <89320177+Yo3lB@users.noreply.github.com>
2021-08-21 23:00:21 +0200

    Add files via upload

---
+++ dmenu/dmenu_aw.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Define the settings for dmenu2 to be used
+
+
+dmenu_run  -p 'BUSCAR' -i -q -o '1.0' -h '22' -fn 'JetBrainsMono Nerd Font-9' -x '137' -y '8' -w '605' -h '18' -nb '#000000' -nf '#8479CB' -sb '#000000' -sf '#4B92FF' 
+
